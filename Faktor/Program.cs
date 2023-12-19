0bc94a9497c3b74a7ca2b856085869162b2a72bd
Br0deNN <theo.broden@gmail.com>
2023-12-19 17:34:36 +0100

    Add project files.

---
+++ Faktor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string x = Console.ReadLine();
+        string[] x2 = x.Split(' ');
+        
+
+        int a = int.Parse(x2[0]);
+        int b = int.Parse(x2[1]);
+
+        int sum = (a * (b - 1));
+        Console.WriteLine(sum + 1);
+
+    }
+}
